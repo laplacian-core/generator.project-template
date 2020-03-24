3d39a1fe1efcffdbe8af9893700805065724d9df
iwauo <iwauo.tajima@gmail.com>
2020-03-24 22:31:03 +0900

    add content

---
+++ scripts/laplacian-template-project-base-publish.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+set -e
+SCRIPT_BASE_DIR=$(cd $"${BASH_SOURCE%/*}" && pwd)
+PROJECT_BASE_DIR=$(cd $SCRIPT_BASE_DIR && cd .. && pwd)
+(cd $PROJECT_BASE_DIR
+  ./gradlew publish
+)
